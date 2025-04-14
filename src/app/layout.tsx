95374763ebeda62bee51e94128958152df86cbfd
seungboshim-iconx <sb.shim@iconx.co.kr>
2025-04-14 15:09:34 +0900

    feat: initial setting

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import localFont from "next/font/local";
+
+const pretendard = localFont({
+  src: "../assets/fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
+
+export const metadata: Metadata = {
+  title: "WagWag Frontend",
+  description: "WagWag Frontend Application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="kr" className={`${pretendard.variable}`}>
+      <body className={`${pretendard.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import Button from "@/components/common/Button";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <Button />
+    </div>
+  );
+}
