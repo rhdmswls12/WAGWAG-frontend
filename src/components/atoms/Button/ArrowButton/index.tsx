fe66839272135ce5f6106f3d7d3f037a2b4fb30e
MOONProd <96318529+MOONProd@users.noreply.github.com>
2025-05-11 11:02:00 +0900

    feat: buttonComponent #WAG 7~8 구현

---
+++ src/components/atoms/Button/ArrowButton/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./ArrowButton.module.scss";
+import { ButtonProps } from "../Button.types";
+
+interface ArrowButtonProps extends ButtonProps {
+  arrowType?: "default" | "line";
+  direction: "left" | "right" | "up" | "down";
+}
+
+const getArrowRotation = (direction: string) => {
+  switch (direction) {
+    case "up":
+      return "rotate(-90deg)";
+    case "down":
+      return "rotate(90deg)";
+    case "left":
+      return "rotate(180deg)";
+    case "right":
+    default:
+      return "none";
+  }
+};
+
+const ArrowButton: React.FC<ArrowButtonProps> = ({
+  arrowType = "default",
+  direction,
+  disabled = false,
+  onClick,
+  ...rest
+}) => {
+  return (
+    <button
+      className={styles.arrowButton + " " + styles[arrowType]}
+      disabled={disabled}
+      onClick={onClick}
+      {...rest}
+    >
+      <img
+        src="/svg/arrow.svg"
+        alt={`${direction} arrow`}
+        className={styles.arrowIcon + " " + styles[`arrow-${direction}`]}
+      />
+    </button>
+  );
+};
+
+export default ArrowButton;
